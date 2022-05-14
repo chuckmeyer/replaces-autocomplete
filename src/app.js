f9492e5adb6f737c95e6fc54c398fd31b9591799
Chuck Meyer <chuck.meyer@algolia.com>
2022-05-14 10:18:01 -0400

    Removed algolia search from form

--- src/app.js
+++ src/app.js
@@ -1,11 +1,7 @@
 import { autocomplete, getAlgoliaResults } from '@algolia/autocomplete-js';
 import { createLocalStorageRecentSearchesPlugin } from '@algolia/autocomplete-plugin-recent-searches';
-import algoliasearch from 'algoliasearch/lite';
 import '@algolia/autocomplete-theme-classic';
 
-const appId = 'latency';
-const apiKey = '6be0576ff61c053d5f9a3225e2a90f76';
-const searchClient = algoliasearch(appId, apiKey);
 const recentSearchesPlugin = createLocalStorageRecentSearchesPlugin({
   key: 'multi-column-layout-example',
   limit: 3,
